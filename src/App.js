481b2d0f128162cb190130d1f0cb6714dbd17cd0
aastha624 <patelaastha2910@gmail.com>
2024-08-28 02:38:19 +0530

    create structure except css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";   
+import Tours from "./Components/Tours";
 
 
 const App = () => {
-  return<div>App</div>;
+  const[tours,settours]=useState(data)// data ui paer dekhadava usestate vaparu
+
+  function removetour(id){
+  const newtours=tours.filter(tour =>tour.id !==id);
+  settours(newtours);
+  }
+  return(
+  <div>
+  
+  <Tours tours={tours} removetour={removetour}></Tours>
+  </div>
+  )
 };
 
 export default App;
--- src/Components/Tours.js
+++ src/Components/Tours.js
@@ -1,24 +1,22 @@
+//tour ma card hase
 import Card from "./Card";
 
-function Tours({ tours, removeTour }) {
-  return (
-    <div className="container">
-      {/* heading */}
-      <div>
-        <h2 className="title">Plan With Love</h2>
-      </div>
-      {/* content */}
-      <div className="cards">
-        {/* here instead of writing different cards for different cities in tours array we use map function */}
-        {/* key={tour.id} => it is a good practice to pass key while using map function */}
-        {
-            tours.map((tour) => {
-            return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
-           })
-        }
-      </div>
+  function Tours({tours,removetour}){
+return(
+  <div>
+    <div>
+      <h2>TravelHeart</h2>
     </div>
-  );
-}
-
-export default Tours;
+    <div>
+      {
+        tours.map((tour)=>{
+          return <Card  {...tour} removetour={removetour}></Card>
+        })//seven city data map karavi lidho indivitual lakhavana badale map use kari lidhu 
+      }
+    </div>
+  </div>
+)
+  }
+  export default Tours;
+  {/* here instead of writing different cards for different cities in tours array we use map function */}
+        {/* key={tour.id} => it is a good practice to pass key while using map function */}
